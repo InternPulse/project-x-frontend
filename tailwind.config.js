f0515db75657f79f1efadd97a384281ce78f283a
Adesoye Abimbola <100973078+BimboAdesoye@users.noreply.github.com>
2024-02-19 22:20:38 +0100

    Set up the project with Vite+React and Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include React components
+    "./index.html", // Include HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
