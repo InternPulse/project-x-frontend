afd57422e6cd8fc993789000bb2192e037cf425a
Adesoye Abimbola <100973078+BimboAdesoye@users.noreply.github.com>
2024-02-21 19:17:20 +0100

    installed flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Include React components
     "./index.html", // Include HTML files
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbitePlugin],
 };
