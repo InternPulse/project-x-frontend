d0906e76a658a4e72f1c5593c7585a3db51d0953
Adesoye Abimbola <100973078+BimboAdesoye@users.noreply.github.com>
2024-02-27 17:34:35 +0100

    implemented the UI for the sign-up and welcome pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Welcome from "./pages/Welcome";
+import "cal-sans";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Welcome" element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
